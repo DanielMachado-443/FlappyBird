a061898bb6af03cd1966d3c0e76f83294867f816
Daniel Machado <daniel.machado.av@gmail.com>
2021-03-14 00:16:05 -0300

    MY adding the background movement, coins, many fixes and speed and score change as the main frames increase, finished. V 1.0

--- jogo.js
+++ jogo.js
@@ -6,6 +6,9 @@ let whichTela = 0;
 let gameScore = 0;
 let gameOver = false;
 
+let firstFlag = false;
+let secondFlag = false;
+
 const globais = {} // << Declared as a null object
 let telaAtiva = {}; // << object declared null
 
@@ -23,7 +26,10 @@ som_GameLevel.src = './efeitos/mixkit-game-level-music-689 (mp3cut.net).wav';
 som_GameLevel.loop = true;
 
 const sprites = new Image(); // << look back to it
-sprites.src = './sprites.png';
+sprites.src = './assets/sprites.png';
+
+const NewSprites = new Image(); // << look back to it
+NewSprites.src = './assets/NewSprites.png';
 
 const canvas = document.querySelector('canvas'); // << Picking up the 'canvas' object
 const contexto = canvas.getContext('2d');
@@ -33,12 +39,12 @@ const contexto = canvas.getContext('2d');
 const planoDeFundo = {
   spriteX: 390,
   spriteY: 0,
-  largura: 275,
-  altura: 204,
+  largura: 276,
+  altura: 176,
   x: 0,
   y: canvas.height - 204, // << We subtract the 'altura' value from canvas.height
   desenha() {
-    contexto.fillStyle = '#2e4482';
+    contexto.fillStyle = '#e158a0';
     contexto.fillRect(0, 0, canvas.width, canvas.height)
 
     contexto.drawImage(
@@ -57,6 +63,37 @@ const planoDeFundo = {
       planoDeFundo.largura, planoDeFundo.altura,
     );
   },
+  atualiza() {
+
+    let movimentoDoPF = 0;
+
+    if (gameScore <= 100) {
+      movimentoDoPF = 1;
+    }
+
+    else if (gameScore > 100 && gameScore <= 300 && firstFlag) { // << THE else if order DOES matter here!!!
+      movimentoDoPF = 2;
+    }
+    else if (gameScore > 100 && gameScore <= 300) {
+      movimentoDoPF = 1;
+    }
+
+    else if (gameScore > 300 && secondFlag) {
+      movimentoDoPF = 3;
+    }
+    else if (gameScore > 300) {
+      movimentoDoPF = 2;
+    }
+
+    const movimentacao = planoDeFundo.x - movimentoDoPF;
+    const deveRepetirNaMetade = planoDeFundo.largura / 2;
+    planoDeFundo.x = movimentacao % deveRepetirNaMetade;  // << INTERESTING, once we are incrementing or decremeting a value (chao.x) that receives the remainder
+    // ^^ of a division in which the denomitor is greater than nominator 
+    // ^^ the remainder will never overtake this numerator value!!!
+    // ^^ Since the remainder of a division of one number by ITSELF is 0
+    // ^^ Whenever chao.x gets 0, it will have the 0 value in the next time the atualiza() function is called   
+    // ^^ OBS: the remainder of a division in which the denomitor is greater than nominator will ALWAYS be the NOMINATOR ITSELF!!!
+  }
 };
 
 // [Chao]
@@ -64,21 +101,10 @@ function criaChao() {
   const chao = {
     spriteX: 0,
     spriteY: 610,
-    largura: 224,
+    largura: 320,
     altura: 112,
     x: 0,
     y: canvas.height - 112,
-    atualiza() {
-      const movimentoDoChao = 1;
-      const deveRepetirNaMetade = chao.largura / 2;
-      const movimentacao = chao.x - movimentoDoChao;
-      chao.x = movimentacao % deveRepetirNaMetade;  // << INTERESTING, once we are incrementing or decremeting a value (chao.x) that receives the remainder
-      // ^^ of a division in which the denomitor is greater than nominator 
-      // ^^ the remainder will never overtake this numerator value!!!
-      // ^^ Since the remainder of a division of one number by ITSELF is 0
-      // ^^ Whenever chao.x gets 0, it will have the 0 value in the next time the atualiza() function is called   
-      // ^^ OBS: the remainder of a division in which the denomitor is greater than nominator will ALWAYS be the NOMINATOR ITSELF!!!                                                
-    },
     desenha() {
       contexto.drawImage(
         sprites,
@@ -91,13 +117,45 @@ function criaChao() {
           sprites,
           chao.spriteX, chao.spriteY,
           chao.largura, chao.altura,
-          (chao.x + chao.largura), chao.y,
+          (chao.x + chao.largura), chao.y, // << To complete chao after the first chao being ended
           chao.largura, chao.altura,
         );
+    },
+    atualiza() {
+
+      let movimentoDoChao = 0;
+
+      if (gameScore <= 100) {
+        movimentoDoChao = 2;
+      }
+
+      else if (gameScore > 100 && gameScore <= 300 && firstFlag) { // << THE else if order DOES matter here!!!
+        movimentoDoChao = 3;
+      }
+      else if (gameScore > 100 && gameScore <= 300) {
+        movimentoDoChao = 2;
+      }
+
+      else if (gameScore > 300 && secondFlag) {
+        movimentoDoChao = 4;
+      }
+      else if (gameScore > 300) {
+        movimentoDoChao = 3;
+      }
+
+      const movimentacao = chao.x - movimentoDoChao;
+      const deveRepetirNaMetade = chao.largura;
+      chao.x = movimentacao % deveRepetirNaMetade;  // << INTERESTING, once we are incrementing or decremeting a value (chao.x) that receives the remainder
+      // ^^ of a division in which the denomitor is greater than nominator 
+      // ^^ the remainder will never overtake this numerator value!!!
+      // ^^ Since the remainder of a division of one number by ITSELF is 0
+      // ^^ Whenever chao.x gets 0, it will have the 0 value in the next time the atualiza() function is called   
+      // ^^ OBS: the remainder of a division in which the denomitor is greater than nominator will ALWAYS be the NOMINATOR ITSELF!!!                                                
     },
   };
   return chao;
 }
+
 // AFTER criaChao() function bellow
 function fazColisaoComOChao(flappyBird, chao) {
   const flappyBirdY = flappyBird.y + flappyBird.altura; // << This 'altura' is filled up by the flappyBoard pixels
@@ -128,7 +186,7 @@ function criaCanos() {
       spriteX: 52,
       spriteY: 169,
     },
-    espaco: 80, // << Space between the 'canos'
+
     desenha() {
       canos.pares.forEach(function (par) { // << DIFFERENT foreach SYNTAX <<< PAY ATTENTION!!!
         const yRandom = -180;
@@ -162,9 +220,9 @@ function criaCanos() {
           x: canoCeuX, // << It will receive the 'par.x' itself just as the 'canoChaoX' had already done before
           y: canoCeuY + canos.altura // << IMPORTANT!!! We need to sum up the 'canos.altura' because canoCeuY begins ABOVE of the canvas position, being a negative value set by 'atualiza()' 
         }
-        par.canoChao = {
+        par.canoChao = { // << 'canoChao.y' IS THE ONLY REASON WHY WE NEED THESE TWO OBJECTS IN THE FIRST PLACE, they will be used in the 'temColisaoComOFlappyBird method
           x: canoChaoX,
-          y: canoChaoY
+          y: canoChaoY // << WE DONT NEED TO CHANGE IT, BECAUSE IT ALREADY BEGGINS IN THE SIDE THAT THE FlappyBird CAN HIT
         }
       })
     },
@@ -184,34 +242,125 @@ function criaCanos() {
     },
     pares: [],
     atualiza() {
-      const passou100Frames = frames % 100 === 0; // << Different SYNTAX ... The '===' will turn the 'passou100Frames' variable from double into a boolean
-      if (passou100Frames) {
-        canos.pares.push({ // << INTERESTING SYNTAX!!!
-          x: canvas.width,
-          y: -150 * (Math.random() + 1),
-        }); // << Pay atention to this SYNTAX
+      if (gameScore <= 100) {
+        const passou100Frames = frames % 100 === 0; // << Different SYNTAX ... The '===' will turn the 'passou100Frames' variable from double into a boolean
+        if (passou100Frames) {
+          canos.pares.push({ // << INTERESTING SYNTAX!!!
+            x: canvas.width,
+            y: -175 * (Math.random() + 1),
+            frameAtPush: frames,
+          }); // << Pay atention to this SYNTAX                  
+        }
+
+        canos.pares.forEach(function (par) {
+          par.x = par.x - 2; // << IT CREATES A SPACE BETWEEN THE 'canos'!!!
+
+          if (canos.temColisaoComOFlappyBird(par)) {
+            console.log('Você perdeu!');
+            gameOver = true; // << Not used yet
+            som_GameLevel.pause();
+            som_HIT.play();
+
+            gameScore = globais.placar.pontuacao;
+            mudaParaTela(Telas.GAME_OVER); // << Reseting the game
+            setTimeout(() => {
+              som_GameOver.play();
+            }, 250);
+          }
+          // << DELETING THE GONE 'canos' BELLOW
+          if (par.x <= 0 - (canos.largura + 3000)) { // << I'VE MADE IT DIFFERENT, BUT IT WORKS AND I PREFFER THIS WAY
+            canos.pares.shift(); // << INTERESTING!!! The shift method removes the first array element, which fits properly in this situation
+          }
+        });
       }
+      else if (gameScore > 100 && gameScore <= 300) {
+        const passou100Frames = frames % 73 === 0; // << Different SYNTAX ... The '===' will turn the 'passou100Frames' variable from double into a boolean  
+        const thatInterval = 100;
 
-      canos.pares.forEach(function (par) {
-        par.x = par.x - 2; // << IT CREATES A SPACE BETWEEN THE 'canos'!!!
+        if (passou100Frames) {
+          let isItTrue = frames >= this.pares[this.pares.length - 1].frameAtPush + thatInterval || firstFlag == true;
+          console.log('Is it true: ', isItTrue);
+          if (isItTrue) {
+            firstFlag = true,
 
-        if (canos.temColisaoComOFlappyBird(par)) {
-          console.log('Você perdeu!');
-          gameOver = true; // << Not used yet
-          som_GameLevel.pause();
-          som_HIT.play();
-
-          gameScore = globais.placar.pontuacao;
-          mudaParaTela(Telas.GAME_OVER); // << Reseting the game
-          setTimeout(() => {
-            som_GameOver.play();
-          }, 250);
+              canos.pares.push({ // << INTERESTING SYNTAX!!!
+                x: canvas.width,
+                y: -175 * (Math.random() + 1),
+                frameAtPush: frames,
+              }); // << Pay atention to this SYNTAX            
+          }
         }
-        // << DELETING THE GONE 'canos' BELLOW
-        if (par.x <= 0 - canos.largura) { // << I'VE MADE IT DIFFERENT, BUT IT WORKS AND I PREFFER THIS WAY
-          canos.pares.shift(); // << INTERESTING!!! The shift method removes the first array element, which fits properly in this situation
+
+        canos.pares.forEach(function (par) {
+
+          if (firstFlag) {
+            par.x = par.x - 3; // << IT CREATES A SPACE BETWEEN THE 'canos'!!!
+          }
+          else {
+            par.x = par.x - 2; // << IT CREATES A SPACE BETWEEN THE 'canos'!!!
+          }
+
+          if (canos.temColisaoComOFlappyBird(par)) {
+            console.log('Você perdeu!');
+            gameOver = true; // << Not used yet
+            som_GameLevel.pause();
+            som_HIT.play();
+
+            gameScore = globais.placar.pontuacao;
+            mudaParaTela(Telas.GAME_OVER); // << Reseting the game
+            setTimeout(() => {
+              som_GameOver.play();
+            }, 250);
+          }
+          // << DELETING THE GONE 'canos' BELLOW
+          if (par.x <= 0 - (canos.largura + 3000)) { // << I'VE MADE IT DIFFERENT, BUT IT WORKS AND I PREFFER THIS WAY
+            canos.pares.shift(); // << INTERESTING!!! The shift method removes the first array element, which fits properly in this situation
+          }
+        });
+      }
+      else {
+        const passou100Frames = frames % 60 === 0; // << Different SYNTAX ... The '===' will turn the 'passou100Frames' variable from double into a boolean        
+        const thatInterval = 100;
+
+        if (passou100Frames) {
+          if (frames >= this.pares[this.pares.length - 1].frameAtPush + thatInterval || secondFlag == true) {
+            secondFlag = true,
+
+              canos.pares.push({ // << INTERESTING SYNTAX!!!
+                x: canvas.width,
+                y: -175 * (Math.random() + 1),
+                frameAtPush: frames,
+              }); // << Pay atention to this SYNTAX
+          }
         }
-      });
+
+        canos.pares.forEach(function (par) {
+
+          if (secondFlag) {
+            par.x = par.x - 4; // << IT CREATES A SPACE BETWEEN THE 'canos'!!!
+          }
+          else {
+            par.x = par.x - 3; // << IT CREATES A SPACE BETWEEN THE 'canos'!!!
+          }
+
+          if (canos.temColisaoComOFlappyBird(par)) {
+            console.log('Você perdeu!');
+            gameOver = true; // << Not used yet
+            som_GameLevel.pause();
+            som_HIT.play();
+
+            gameScore = globais.placar.pontuacao;
+            mudaParaTela(Telas.GAME_OVER); // << Reseting the game
+            setTimeout(() => {
+              som_GameOver.play();
+            }, 250);
+          }
+          // << DELETING THE GONE 'canos' BELLOW
+          if (par.x <= 0 - (canos.largura + 3000)) { // << I'VE MADE IT DIFFERENT, BUT IT WORKS AND I PREFFER THIS WAY
+            canos.pares.shift(); // << INTERESTING!!! The shift method removes the first array element, which fits properly in this situation
+          }
+        });
+      }
     }
   }
   return canos;
@@ -301,7 +450,7 @@ function criaFlappyBird() { // << Like a constructor, now we can create a pre de
   return flappyBird;
 }
 
-function criaPlacar() {
+function criaPlacar() { // << Is it necessary to be a function?
   const placar = {
     pontuacao: 0,
     desenha() {
@@ -311,11 +460,22 @@ function criaPlacar() {
       contexto.fillText(`Score: ${placar.pontuacao}`, canvas.width - 10, 35); // << Look back to this SYNTAX
     },
     atualiza() {
-      const intervaloDeFrames = 10;
+      const intervaloDeFrames = 10; // << SCORE
       const passouOIntervalo = frames % intervaloDeFrames === 0; // << this 'frames' and 'frameAtual' are NOT the same thing ... WHY DOES CONST WORK HERE?
 
       if (passouOIntervalo && !fazColisaoComOChao(globais.flappyBird, globais.chao)) {
-        placar.pontuacao = placar.pontuacao + 1;
+        if (gameScore <= 100) {
+          placar.pontuacao = placar.pontuacao + 1;
+          gameScore = placar.pontuacao;
+        }
+        else if (gameScore > 100 && gameScore <= 300) {
+          placar.pontuacao = placar.pontuacao + (1 * 2);
+          gameScore = placar.pontuacao;
+        }
+        else {
+          placar.pontuacao = placar.pontuacao + (1 * 3);
+          gameScore = placar.pontuacao;
+        }
       }
     }
   }
@@ -356,6 +516,46 @@ const mensagemGetReady = {
   }
 }
 
+// [Coins]
+const Coins = {
+  spriteX: 0,
+  spriteY: 124, // Golden coin position
+  largura: 44,
+  altura: 44,
+  x: ((canvas.width / 2) - 226 / 2) + 27,
+  y: ((canvas.height / 2) - (200 / 2)) + 87,
+  desenha() {
+
+    if (gameScore <= 200) {
+      contexto.drawImage(
+        NewSprites,
+        48, 124,
+        Coins.largura, Coins.largura, // << It doesnt matter if whether I use 'largura' or 'altura' because this picture is a square
+        Coins.x, Coins.y,
+        Coins.largura, Coins.largura,
+      );
+    }
+    else if (gameScore > 200 && gameScore <= 400) {
+      contexto.drawImage(
+        NewSprites,
+        48, 78,
+        Coins.altura, Coins.altura, // << It doesnt matter if whether I use 'largura' or 'altura' because this picture is a square
+        Coins.x, Coins.y,
+        Coins.altura, Coins.altura,
+      );
+    }
+    else {
+      contexto.drawImage(
+        NewSprites,
+        Coins.spriteX, Coins.spriteY,
+        Coins.largura, Coins.altura, // << It doesnt matter if whether I use 'largura' or 'altura' because this picture is a square
+        Coins.x, Coins.y,
+        Coins.largura, Coins.altura,
+      );
+    }
+  },
+};
+
 /// [GAME_OVER]
 const mensagemGameOver = {
   sX: 134,
@@ -371,7 +571,8 @@ const mensagemGameOver = {
       mensagemGameOver.w, mensagemGameOver.h,
       mensagemGameOver.x, mensagemGameOver.y,
       mensagemGameOver.w, mensagemGameOver.h,
-    )
+    ),
+      Coins.desenha();
   },
   atualiza() {
     contexto.font = '28px "VT323"',
@@ -414,7 +615,6 @@ const Telas = {
       mudaParaTela(Telas.JOGO);
     },
     atualiza() {
-      globais.chao.atualiza(); // << Its necessary to update the chao, because its moving      
     },
   }
 };
@@ -425,6 +625,9 @@ Telas.JOGO = { // << Declaring a Telas attribute (JOGO) in runtime, AFTER the Te
       som_GameLevel.play();
     globais.placar = criaPlacar();
     globais.flappyBird.pula();
+
+    firstFlag = false;
+    secondFlag = false;
   },
   desenha() {
     planoDeFundo.desenha(); // << WONDERFUL -> CHANGING THE 'desenha' METHOD CALL ORDER MAKE A PICTURE FROM THE SPRITE OVERRIDE ANOTHER
@@ -437,6 +640,7 @@ Telas.JOGO = { // << Declaring a Telas attribute (JOGO) in runtime, AFTER the Te
     globais.flappyBird.pula(); // << 'pula' method returns the final time                       
   },
   atualiza() {
+    planoDeFundo.atualiza();
     globais.canos.atualiza();
     globais.chao.atualiza();
     globais.flappyBird.atualiza();
@@ -447,7 +651,7 @@ Telas.JOGO = { // << Declaring a Telas attribute (JOGO) in runtime, AFTER the Te
 Telas.GAME_OVER = {
   desenha() {
     whichTela = 2,
-    planoDeFundo.desenha(); // << WONDERFUL -> CHANGING THE 'desenha' METHOD CALL ORDER MAKE A PICTURE FROM THE SPRITE OVERRIDE ANOTHER
+      planoDeFundo.desenha(); // << WONDERFUL -> CHANGING THE 'desenha' METHOD CALL ORDER MAKE A PICTURE FROM THE SPRITE OVERRIDE ANOTHER
     globais.canos.desenha();
     globais.chao.desenha();
     globais.flappyBird.desenha(); // << flappyBird maximum drawing priority     
